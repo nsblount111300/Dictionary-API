c08e5383ba3babe54295c48846b5d15c498dd1fd
nsblount111300 <nsblount111300@gmail.com>
2022-10-31 01:18:07 -0400

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const url = require("url");
 const sqlite3 = require("sqlite3").verbose();
